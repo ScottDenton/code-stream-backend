7d4f2a29e40f71f53d8c4cc89236f6d69541809c
ScottDenton <scottyd_14@hotmail.com>
2019-04-17 19:43:40 -0700

    Sessions controller cleaned up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,15 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     @user = User.all.find_by(username: params[:username])
-      render json: @user
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/', notice: "Logged Out"
+    render json: @user
   end
 
   def getUserVideos
     url = "https://api.twitch.tv/helix/videos?user_id=#{params[:twitch_id]}"
-    headers={
-      'Client-ID': Rails.application.credentials.twitch[:secret_api_key]
-    }
-
+    headers={'Client-ID': Rails.application.credentials.twitch[:secret_api_key]}
     response = HTTParty.get(url, headers: headers)
     @data= response.body
-
     render json: @data
-
   end
 
 end
